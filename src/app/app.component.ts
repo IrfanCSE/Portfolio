23d3722de459c82f60a5a399837b03cd33c854a3
IrfanCSE <irfanulhasan1996@gmail.com>
2022-03-10 00:26:58 +0600

    update content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,16 @@ import { environment } from 'src/environments/environment';
 })
 export class AppComponent {
   title = 'Irfanul Hasan';
+  env = environment.production;
+  workTab = "panel-0";
+  href = "";
+  constructor() {
+    if(this.env){
+      this.href = "Portfolio";
+    }
+  }
+
+  funWorkTab(name:string): void{
+    this.workTab = name;
+  }
 }
